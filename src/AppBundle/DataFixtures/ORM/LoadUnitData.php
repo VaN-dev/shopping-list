26bcb0a34456ff8805a4fdbf70f2eea249e1804f
David Micheau <fanel.dev@gmail.com>
2017-04-16 16:52:47 +0200

    fixtures

--- src/AppBundle/DataFixtures/ORM/LoadUnitData.php
+++ src/AppBundle/DataFixtures/ORM/LoadUnitData.php
@@ -38,6 +38,41 @@ class LoadUnitData implements FixtureInterface
         ;
         $manager->persist($scope_03);
 
+        $scope_04 = new Unit();
+        $scope_04
+            ->setName('boîte')
+            ->setSlug('boite')
+        ;
+        $manager->persist($scope_04);
+
+        $scope_05 = new Unit();
+        $scope_05
+            ->setName('cuillère à soupe')
+            ->setSlug('cuillere-a-soupe')
+        ;
+        $manager->persist($scope_05);
+
+        $scope_06 = new Unit();
+        $scope_06
+            ->setName('cuillère à café')
+            ->setSlug('cuillere-a-cafe')
+        ;
+        $manager->persist($scope_06);
+
+        $scope_07 = new Unit();
+        $scope_07
+            ->setName('sachet')
+            ->setSlug('sachet')
+        ;
+        $manager->persist($scope_07);
+
+        $scope_08 = new Unit();
+        $scope_08
+            ->setName('tranche')
+            ->setSlug('tranche')
+        ;
+        $manager->persist($scope_08);
+
         $manager->flush();
     }
 }
