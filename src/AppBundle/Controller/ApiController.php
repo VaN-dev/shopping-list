46e43e0afcf4592608105018241559e1f86aedea
David Micheau <david.micheau@upnbiz.com>
2017-09-14 14:35:01 +0200

    wip

--- src/AppBundle/Controller/ApiController.php
+++ src/AppBundle/Controller/ApiController.php
@@ -3,6 +3,8 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Class ApiController
@@ -10,6 +12,91 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
  */
 class ApiController extends Controller
 {
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function resolveSpeechAction(Request $request)
+    {
+//        $client = new \GuzzleHttp\Client();
+//
+//        $query = $_POST['query']; //the users query
+//
+//        $send = [
+//            'headers' => [
+//                'Content-Type' => 'application/json;charset=utf-8',
+//                'Authorization' => 'Bearer '.$client_access_token
+//            ],
+//            'body' => json_encode([
+//                'query' => $query,
+//                'lang' => 'en',
+//            ])
+//        ];
+//
+//        try {
+//            $response = $client->post('https://api.api.ai/v1/query?v=20150910', $send);
+//            $result = json_decode($response->getBody(),true);
+//
+//            dump($result);
+//            die();
+//        } catch (\Exception $e) {
+//            dump($e->getMessage());
+//            die();
+//        }
+
+        $query = $request->query->get("q");
+
+        $client = $this->get('app.api.client');
+        $result = $client->query($query);
+        
+//        dump($result);
+//        die();
+
+
+
+        if(!empty($result->result) && !empty($result->result->action)){
+            switch ($result->result->action) {
+                case 'smalltalk.greetings.hello':
+                    $response = [
+                        'success' => true,
+                        'action' => 'smalltalk',
+                        'speech' => $result->result->fulfillment->speech,
+                    ];
+                    break;
+                case 'redirect':
+                    $params = (array) $result->result->parameters;
+                    unset($params['route']);
+                    $target = str_replace(" ", "", $this->generateUrl($result->result->parameters->route, $params));
+                    $response = [
+                        'success' => true,
+                        'action' => 'Redirection vers ' . $target,
+                        'speech' => null,
+                        'params' => [
+                            'action' => $result->result->action,
+                            'value' => $target,
+                        ]
+                    ];
+
+                    $request->getSession()->getFlashBag()->add('speech', $result->result->fulfillment->speech);
+                    break;
+
+                default:
+                    $response = [
+                        'success' => false,
+                        'action' => 'Action non-reconnue',
+                        'speech' => 'Action non reconnue',
+                        'params' => null,
+                    ];
+                    break;
+            }
+
+            dump($response);
+            die();
+
+            return new JsonResponse($response);
+        }
+    }
+
     /**
      * @return \Symfony\Component\HttpFoundation\Response
      */
--- src/AppBundle/Service/ApiClient/ApiClient.php
+++ src/AppBundle/Service/ApiClient/ApiClient.php
@@ -41,6 +41,8 @@ class ApiClient
      */
     private $version = 20150910;
 
+    private $lang = "fr";
+
     /**
      * ApiClient constructor.
      * @param SessionInterface $session
@@ -51,18 +53,61 @@ class ApiClient
     {
         $this->session = $session;
         $this->client = new Client([
-            "base_uri" => $this->base_uri,
-            "verify" => false,
+//            "base_uri" => $this->base_uri,
+//            "verify" => false,
         ]);
 
         $this->client_access_token = $client_access_token;
         $this->developer_access_token = $developer_access_token;
     }
 
-    public function createUserEntity()
+    public function query($query)
     {
         $body = [
             "sessionId" => $this->session->getId(),
+            "lang" => $this->lang,
+            "query" => $query,
+        ];
+
+        $headers = [
+            "Authorization" => "Bearer " . $this->client_access_token,
+            "Content-type" => "application/json;charset=UTF-8",
+            'Accept' => 'application/json',
+        ];
+
+        foreach ($headers as $k => $header) {
+            $headers[] = $k . ": " . $header;
+            unset($headers[$k]);
+        }
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $this->base_uri . "/query?v=" . $this->version,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => json_encode($body),
+            CURLOPT_HTTPHEADER => $headers,
+            CURLOPT_RETURNTRANSFER => true,
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+            die();
+        }
+
+        return json_decode($response);
+    }
+
+    public function createUserEntity()
+    {
+        $body = [
+//            "sessionId" => $this->session->getId(),
+            "sessionId" => 12345,
             "entities" => [
                 [
                     "name" => "recipe",
@@ -79,28 +124,44 @@ class ApiClient
         ];
         $json = json_encode($body);
 
-        $curl = curl_init();
+        $headers = [
+            "Authorization" => "Bearer " . $this->developer_access_token,
+            "Content-type" => "application/json;charset=UTF-8",
+            'Accept' => 'application/json',
+        ];
 
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => $this->base_uri . "/userEntities/?v=" . $this->version,
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => $json,
-            CURLOPT_HTTPHEADER => array(
-                "authorization: Bearer " . $this->developer_access_token,
-                "content-type: application/json;charset=utf-8",
-            ),
-        ));
+//        $curl = curl_init();
+//
+//        curl_setopt_array($curl, array(
+//            CURLOPT_URL => $this->base_uri . "/userEntities/?v=" . $this->version,
+//            CURLOPT_CUSTOMREQUEST => "POST",
+//            CURLOPT_POSTFIELDS => $json,
+//            CURLOPT_HTTPHEADER => array(
+//                "authorization: Bearer " . $this->developer_access_token,
+//                "content-type: application/json;charset=utf-8",
+//            ),
+//        ));
+//
+//        $response = curl_exec($curl);
+//        $err = curl_error($curl);
+//
+//        curl_close($curl);
+//
+//        if ($err) {
+//            echo "cURL Error #:" . $err;
+//        } else {
+//            echo $response;
+//        }
 
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
 
-        curl_close($curl);
-
-        if ($err) {
-            echo "cURL Error #:" . $err;
-        } else {
-            echo $response;
-        }
+        echo '<pre>';
+        $response = $this->client->post($this->base_uri . '/userEntities?v=' . $this->version, [
+            'headers'       => $headers,
+            'form_params'   => $body,
+            'debug'         => true,
+        ]);
+        echo '</pre>';
+        dump(json_decode($response->getBody(), true));
 
         die();
 
